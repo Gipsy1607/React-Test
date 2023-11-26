319c2d08196334c3fe3cc99eedcdeca25b7ae1d0
Gipsy1607 <swarangitarkar@gmail.com>
2023-11-26 18:09:35 +0530

    Updates

---
+++ practise.js
@@ -0,0 +1,9 @@
+create an 3*3 grid in which you move either down or right and you are on the top cell but it will also calculate the paths taken by the us:
+
+function createGrid(rows, columns){
+    var grid = [];
+    for(i=0, i<=rows.length, i++)
+}
+createGrid(3,3);
+
+
---
+++ src/Components/Paths.jsx
@@ -0,0 +1,5 @@
+function Path (){
+    
+}
+
+export default Path
